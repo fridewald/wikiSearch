dbd52d34ad3edc94e8c04de0b747cb3aa586621f
Julian Lukwata <julianlukwata@gmail.com>
2017-03-09 23:09:32 +0100

    working result

---
+++ main.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""main file for wikiSearch"""
+import os
+
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, URL
+from flask_sqlalchemy import SQLAlchemy
+
+import reSearch
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+# set key for some security functions
+app.config['SECRET_KEY'] = 'ansdaerofbcp348p2M432023ML+#th5'
+# SQLAlchemy configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# cross-site request forgery protection
+csrf = CSRFProtect(app)
+# bootstrap for neat design
+bootstrap = Bootstrap(app)
+# for easy run
+manager = Manager(app)
+# SQLite database object
+db = SQLAlchemy(app)
+
+class UrlForm(FlaskForm):
+    """Wikipedia form"""
+    url = StringField('Enter a Wikipedia Url. May take some time to find him.', validators=[Required(), URL(message=u'Invalid URL.')])
+    submit = SubmitField('Search')
+
+class Distance(db.Model):
+    """SQL Database for the distance to him"""
+    __tablename__ = 'distance'
+    id = db.Column(db.Integer, primary_key=True)
+    distance = db.Column(db.Integer, unique=True)
+    urls = db.relationship('UrlDb', backref='distance', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Distance %r>' % self.distance
+
+
+class UrlDb(db.Model):
+    """SQL Database for found urls"""
+    __tablename__ = 'urls'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Unicode, unique=True)
+    name = db.Column(db.Unicode(64))
+    road = db.Column(db.Unicode)
+    distance_id = db.Column(db.Integer, db.ForeignKey('distance.id'))
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UrlForm()
+    if form.validate_on_submit():
+        session['url'] = form.url.data
+        url_obj = UrlDb.query.filter_by(url=session['url']).first()
+        try:
+            print(url_obj.road)
+        except:
+            print('No object defined')
+        if url_obj:
+            session['road'] = (url_obj.road
+                               + '. Distance = ' + str(url_obj.distance.distance))
+        else:
+            result_list = reSearch.hit_search(session['url'])
+            session['road'] = (result_list[0].road
+                              + '. Distance = ' + str(result_list[0].distance))
+            store_result(result_list)
+        return redirect(url_for('index'))
+    road = session.get('road', None)
+    try:
+        del session['road']
+    except:
+        pass
+    return render_template('index.html', form=form, result=road)
+
+@app.route('/results')
+def result():
+    return render_template('result.hmtl')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+def store_result(result_list):
+    for res in result_list:
+        distance = Distance.query.filter_by(distance=res.distance).first()
+        if not distance:
+            distance = Distance(distance=res.distance)
+            db.session.add(distance)
+        print(res.url)
+        url = UrlDb.query.filter_by(url=res.url).first()
+        if not url:
+            url = UrlDb(url=res.url, name=res.heading, road=res.road, distance=distance)
+            db.session.add(url)
+        #print(res.heading)
+        #print(res.road)
+        #print(res.distance)
+    db.session.commit()
+
+if __name__ == '__main__':
+    db.create_all()
+    manager.run()
--- reSearch.py
+++ reSearch.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
 import urllib2
-from bs4 import BeautifulSoup, SoupStrainer
 import re
 import argparse
+import timeit
+
+from bs4 import BeautifulSoup, SoupStrainer
 
 #TODO also for English and other languages
 HITLINK = "/wiki/Adolf_Hitler"
 
 class wiki_site():
     """class for searching him"""
-    def __init__(self, url, level=0, **kwargs):
+    def __init__(self, url, **kwargs):
         # the url of the current site
         self.url = url
         self.links = None
         self.heading = ''
+        self.clicked_name = kwargs.get('clicked_name', self.get_heading())
         # Text of the site already visited
-        self.road = kwargs.get('road', '')
+        #self.road = kwargs.get('road', '')
+        self.road = ''
         # how many sites have already been visited
-        self.level = level
-        #
+        #self.level = level
+        # how many links have to be clicked
         self.distance = None
+        #
+        self.country_string = re.search('(?<=https://)\w+(?=\.wikipedia.org)', url).group(0)
 
     def load_soup(self):
         if not self.links:
@@ -30,11 +36,10 @@ class wiki_site():
             # BeautifulSoup object that stores site
             soup = BeautifulSoup(html, 'lxml')
             # Masterpiece...not,
-            #TODO other languages, and maybe also these Datei|Hilfe|...
-            self.links = [li.get('href') for li in soup.findAll('a', attrs={'href': re.compile('^/wiki/(?!(Datei|Hilfe|Spezial|Wikipedia|Portal|Kategorie|Diskussion):).+$((?<!png)|(?<!jpg))')})]
-            #self.links = [li.get('href') for li in soup.findAll('a', attrs={'href': re.compile('^/w')})]
-            self.links = list(set(self.links))
-            self.heading = soup.find(id='firstHeading').string
+            self.links, self.link_names = zip(*[[li.get('href'), ''.join(li.stripped_strings)] for li in soup.findAll('a', attrs={'href': re.compile('^/wiki/(?!\w+:).+$((?<!png)|(?<!jpg))')})])
+            #self.links = list(set(self.links))
+            self.heading = ''.join(soup.find(id='firstHeading').stripped_strings)
+            #self.heading = re.search('(?=/wiki/)\w+', self.url)
 
     def get_wiki_links(self):
         """get all internal links"""
@@ -47,74 +52,112 @@ class wiki_site():
         return self.heading
 
 
-def hit_gen(url):
-    """masterpiece generator, for recursive search for him"""
-    site = wiki_site(url)
+def hit_gen(url, links_gen, **kwargs):
+    """
+    masterpiece generator, for recursive search for him
+
+    args:
+        url: real Url
+        kwargs[link_name]: the name the link shown on site
+    """
+    # an wiki_site obj
+    site = wiki_site(url, **kwargs)
     second_part_links = site.get_wiki_links()
-    first_part_links = 'https://de.Wikipedia.org'
+    link_names = site.link_names
+    # 2D-list with the links and the link-names
+    #second_part_links_and_link_names = site.get_wiki_links()
+    # the first part of the wikipedia url
+    first_part_links = 'https://' + site.country_string + '.wikipedia.org'
+    # somebody has entered his site directly
+    if url == first_part_links + HITLINK:
+        site.distance = 0
+        site.road = '"' + site.get_heading() + '". Ziemlich schlau direkt seine Seite einzugeben'
+        yield [site]
+        return
     # first search on site
-    for spl in second_part_links:
+    print('Searching in ' + url)
+    for spl, li_name in zip(second_part_links, link_names):
+        # found him?
         if spl == HITLINK:
-            yield [url, first_part_links + spl]
+            hit_site = wiki_site(first_part_links + HITLINK, link_name=li_name)
+            hit_site.get_heading()
+            hit_site.distance = 0
+            site.distance = 1
+            hit_site.road = '"' + hit_site.heading + '". Ziemlich schlau direkt seine Seite einzugeben'
+            site.road = '"' + site.clicked_name + '" dann "' + hit_site.clicked_name + '"'
+            yield [site, hit_site]
             return
-    # Nothing found so None
-    print('Nothing found in ' + url)
+    # Nothing found
     yield None
-    # then search in all links untill he is found
-    gen_list = [hit_gen(first_part_links + spl) for spl in second_part_links]
+    print('Going one layer deeper')
+    # add links to set in links_handle and receive the already handled
+    #handled_links = links_gen.send(second_part_links)
+    # remove all handled links
+    #second_part_links.difference_update(handled_links)
+    # search in all links untill he is found
+    gen_list = [hit_gen(first_part_links + spl, links_gen, clicked_name=li_name) for spl, li_name in zip(second_part_links, link_names)]
+    #print('and here')
     tmp_result = None
     while tmp_result is None:
         for gen in gen_list:
             tmp_result = next(gen)
             if tmp_result:
-                tmp_result.insert(0, url)
+                #print(site.clicked_name)
+                #print(tmp_result[0].road)
+                site.road = '"' + site.clicked_name + '" dann ' + tmp_result[0].road
+                site.distance = tmp_result[0].distance + 1
+                tmp_result.insert(0, site)
                 yield tmp_result
                 return
         # Nothing found, so ge to next level
+        #print('and here too')
         yield None
 
 def links_handle():
-    old_links, new_links = set()
+    """stores all links already handled in a set"""
+    old_links = new_links = old_links_tmp = set()
     while True:
-        new_links = yield old_links
+        #TODO not so good
+        new_links = yield old_links_tmp
+        old_links_tmp = old_links.copy()
         old_links.update(new_links)
+        #print("I'm here")
 
 
 def main():
     parser = my_parser()
     args = parser.parse_args()
-    url1 = 'https://de.wikipedia.org/wiki/Hambach_an_der_Weinstra%C3%9Fe'
+    url4 = 'https://de.wikipedia.org/wiki/Hambach_an_der_Weinstra%C3%9Fe'
     url2 = 'https://de.wikipedia.org/wiki/Stra%C3%9Fburg'
     url3 = 'https://de.wikipedia.org/wiki/Neustadt_an_der_Weinstra%C3%9Fe'
-    url4 = 'https://de.wikipedia.org/wiki/Wasserstoff'
+    url1 = 'https://de.wikipedia.org/wiki/Wasserstoff'
     url = 'https://de.wikipedia.org/wiki/Ettore_Majorana'
-    #HamWiki = wikiSite(url)
-    #print(HamWiki.get_wiki_links())
-    #print(HamWiki.get_heading())
-    #wList, dist = hiSearch(HamWiki)
+    res = hit_search(args.url if args.url else url)
+    print('Ich habe ihn gefunden. Klicke ' + res[0].road)
+
+def hit_search(url):
     total_links = links_handle()
     total_links.send(None)
-    total_hit_generator = hit_gen(args.url if args.url else url)
+    total_hit_generator = hit_gen(url, total_links)
+    result = None
     for res in total_hit_generator:
         if res:
-            out = ''
-            for lin in res:
-                out += ', ' + lin
-            print('Er wurde gefunden' + out)
+            result = res
             break
-    #if wList:
-    #    print("Er wurde gefunden. %s\n" % wList[-1].road)
-    #    print("Es wurden %s Links geclickt" % wList[-1].distance)
+    return res
 
 def my_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', type=str,
+    parser.add_argument('-url', type=str,
                         help=('The wikipedia url to search from,'
                               + 'right now only german'))
     return parser
 
 if __name__ == '__main__':
+    start = timeit.default_timer()
     main()
+    end = timeit.default_timer()
+    print(end - start)
 
 #def hi_search(wSite):
 #    """recursive search for him"""
